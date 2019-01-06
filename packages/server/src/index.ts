1fba8b31b68b153c4b28160f4d894130e6076a46
Sm03leBr00t <Sm03leBr00t@protonmail.com>
2019-01-06 13:03:15 +0100

    some setup

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -8,26 +8,34 @@ import * as session from "express-session";
 import * as connectRedis from "connect-redis";
 import * as cors from "cors";
 import * as Redis from "ioredis";
-import chalk from 'chalk';
+import chalk from "chalk";
 
 const RedisStore = connectRedis(session as any);
 
 const startServer = async () => {
-  console.log(chalk.yellow('[*] Starting up server ...'));
+  console.log(chalk.yellow("[*] Starting up server ..."));
 
-  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Creating database connection ...')}`));
+  process.stdout.write(
+    chalk.yellow(`\t+ ${chalk.white("Creating database connection ...")}`)
+  );
   await createConnection();
-  console.log(chalk.green.bold('DONE'));
+  console.log(chalk.green.bold("DONE"));
 
-  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Creating express app ...')}`));
+  process.stdout.write(
+    chalk.yellow(`\t+ ${chalk.white("Creating express app ...")}`)
+  );
   const app = express();
-  console.log(chalk.green.bold('DONE'));
+  console.log(chalk.green.bold("DONE"));
 
-  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Creating redis client instance ...')}`));
+  process.stdout.write(
+    chalk.yellow(`\t+ ${chalk.white("Creating redis client instance ...")}`)
+  );
   const redis = new Redis();
-  console.log(chalk.green.bold('DONE'))
+  console.log(chalk.green.bold("DONE"));
 
-  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Creating apollo server instance ...')}`));
+  process.stdout.write(
+    chalk.yellow(`\t+ ${chalk.white("Creating apollo server instance ...")}`)
+  );
   const server = new ApolloServer({
     schema: await buildSchema({
       resolvers: [__dirname + "/modules/**/resolver.*"]
@@ -38,18 +46,18 @@ const startServer = async () => {
       redis
     })
   });
-  console.log(chalk.green.bold('DONE'))
+  console.log(chalk.green.bold("DONE"));
 
-  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Using cors ...')}`))
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white("Using cors ...")}`));
   app.use(
     cors({
       credentials: true,
       origin: "http://localhost:4000"
     })
   );
-  console.log(chalk.green.bold('DONE'))
+  console.log(chalk.green.bold("DONE"));
 
-  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Using session ...')}`))
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white("Using session ...")}`));
   app.use(
     session({
       store: new RedisStore({
@@ -65,17 +73,34 @@ const startServer = async () => {
       }
     })
   );
-  console.log(chalk.green.bold('DONE'))
-  
-  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Applying middleware to ApolloServer ...')}`))
-  server.applyMiddleware({ app });
-  console.log(chalk.green.bold('DONE'))
+  console.log(chalk.green.bold("DONE"));
 
-  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Finishing ...')}`))
+  process.stdout.write(
+    chalk.yellow(
+      `\t+ ${chalk.white("Applying middleware to ApolloServer ...")}`
+    )
+  );
+  server.applyMiddleware({ app });
+  console.log(chalk.green.bold("DONE"));
+
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white("Finishing ...")}`));
   app.listen({ port: process.env.PORT || 4000 }, () => {
-    console.log(chalk.green.bold('DONE'))
-    console.log(chalk.yellow(`[${chalk.green.bold('!')}] ${chalk.green.bold(`Server started    \t[http://localhost:${process.env.PORT || 4000}]`)}`));
-    console.log(chalk.yellow(`[${chalk.magenta.bold('+')}] ${chalk.magenta.bold(`GraphQL Playground \t[http://localhost:${process.env.PORT || 4000}/graphql]`)}`));
+    console.log(chalk.green.bold("DONE"));
+    console.log(
+      chalk.yellow(
+        `[${chalk.green.bold("!")}] ${chalk.green.bold(
+          `Server started    \t[http://localhost:${process.env.PORT || 4000}]`
+        )}`
+      )
+    );
+    console.log(
+      chalk.yellow(
+        `[${chalk.magenta.bold("+")}] ${chalk.magenta.bold(
+          `GraphQL Playground \t[http://localhost:${process.env.PORT ||
+            4000}/graphql]`
+        )}`
+      )
+    );
   });
 };
 
