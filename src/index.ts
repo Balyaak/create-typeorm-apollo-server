6243ed33c6b357588623a14b263ed3f337ea23c1
Sm03leBr00t <Sm03leBr00t@protonmail.com>
2019-01-06 01:21:39 +0100

    chalking

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,26 @@ import * as session from "express-session";
 import * as connectRedis from "connect-redis";
 import * as cors from "cors";
 import * as Redis from "ioredis";
+import chalk from 'chalk';
 
 const RedisStore = connectRedis(session as any);
 
 const startServer = async () => {
+  console.log(chalk.yellow('[*] Starting up server ...'));
+
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Creating database connection ...')}`));
   await createConnection();
+  console.log(chalk.green.bold('DONE'));
 
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Creating express app ...')}`));
   const app = express();
+  console.log(chalk.green.bold('DONE'));
 
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Creating redis client instance ...')}`));
   const redis = new Redis();
+  console.log(chalk.green.bold('DONE'))
 
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Creating apollo server instance ...')}`));
   const server = new ApolloServer({
     schema: await buildSchema({
       resolvers: [__dirname + "/modules/**/resolver.*"]
@@ -28,14 +38,18 @@ const startServer = async () => {
       redis
     })
   });
+  console.log(chalk.green.bold('DONE'))
 
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Using cors ...')}`))
   app.use(
     cors({
       credentials: true,
       origin: "http://localhost:3000"
     })
   );
+  console.log(chalk.green.bold('DONE'))
 
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Using session ...')}`))
   app.use(
     session({
       store: new RedisStore({
@@ -51,11 +65,16 @@ const startServer = async () => {
       }
     })
   );
-
+  console.log(chalk.green.bold('DONE'))
+  
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Applying middleware to ApolloServer ...')}`))
   server.applyMiddleware({ app });
+  console.log(chalk.green.bold('DONE'))
 
-  app.listen({ port: 4000 }, () => {
-    console.log("server online");
+  process.stdout.write(chalk.yellow(`\t+ ${chalk.white('Finishing ...')}`))
+  app.listen({ port: process.env.PORT || 4000 }, () => {
+    console.log(chalk.green.bold('DONE'))
+    console.log(chalk.yellow(`[${chalk.green.bold('!')}] ${chalk.green.bold(`Server started [http://localhost:${process.env.PORT || 4000}]`)}`));
   });
 };
 
